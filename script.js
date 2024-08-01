6db72589f3e98c2257a760cd1010a192caada149
Jess Barnes <jessicabarnes@ufl.edu>
2024-08-01 17:14:44 -0400

    Added javascript file for animation of about us section

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(){const aboutSection = document.querySelector(".about-section");
+
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry=> {
+	if (entry.isIntersecting) {
+			entry.target.classList.add("slide-in");
+			observer.unobserve(entry.target);
+			}
+		});
+	});
+	observer.observe(aboutSection);
+});
