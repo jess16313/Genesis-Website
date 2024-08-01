765eb1d7af28b832e5a7412fed3ac984d504af94
Jess Barnes <jessicabarnes@ufl.edu>
2024-08-01 19:29:30 -0400

    Added video and fixed sliding animation

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(){const aboutSection = document.querySelector(".about-section");
+document.addEventListener("DOMContentLoaded", function() {
+    const aboutSection = document.querySelector(".about-section");
 
-const observer = new IntersectionObserver(entries => {
-	entries.forEach(entry=> {
-	if (entry.isIntersecting) {
-			entry.target.classList.add("slide-in");
-			observer.unobserve(entry.target);
-			}
-		});
-	});
-	observer.observe(aboutSection);
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("slide-in");
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    observer.observe(aboutSection);
 });
